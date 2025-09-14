2ce8c9198bee375bea513e137ff9dd1b773c88d5
EZ1712 <rizakistinafiyan@gmail.com>
2025-09-14 09:50:42 +0700

    update

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ int main() {
     char aritmatika;
 
     cout << "Quantum Calculator \n \n";
+    cout << "masukan nilai pertama : ";
+    cin >> a;
+    cout << "masukan operator : ";
+    cin >> aritmatika;
+    cout << "masukan nilai kedua : ";
+    cin >> b;
+    cout << "hasilnya : \n";
+
 
     return 0;
 }
